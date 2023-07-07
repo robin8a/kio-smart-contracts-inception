ec0daca25e03026ef4fa39cdeac2ae4390466dab
Robin Ochoa <robin8a@gmail.com>
2023-07-07 15:06:34 -0500

    update

--- 0010_MIGRATION/00_backup_updload.sh
+++ 0010_MIGRATION/00_backup_updload.sh
@@ -4,9 +4,40 @@ set -eo pipefail
 # https://stackoverflow.com/questions/12241161/copying-one-table-to-another-in-dynamodb
 
 # tables
-TABLE_FROM=Category-d6sfnut2zrdljfaz3ysbjfhvke-dev
-TABLE_TO=Category-csbpzicidrh7vmfyrsgft53qvm-prod
-
+# TABLE_FROM=Category-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Category-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Document-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Document-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Feature-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Feature-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=FeatureType-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=FeatureType-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Formula-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Formula-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Image-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Image-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Order-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Order-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=ProductFeatureResult-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=ProductFeatureResult-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=ProductFeature-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=ProductFeature-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Product-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Product-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Result-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Result-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Transactions-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Transactions-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=UnitOfMeasure-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=UnitOfMeasure-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=UserProduct-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=UserProduct-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=User-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=User-csbpzicidrh7vmfyrsgft53qvm-prod
+# TABLE_FROM=Verification-d6sfnut2zrdljfaz3ysbjfhvke-dev
+# TABLE_TO=Verification-csbpzicidrh7vmfyrsgft53qvm-prod
+TABLE_FROM=Wallet-d6sfnut2zrdljfaz3ysbjfhvke-dev
+TABLE_TO=Wallet-csbpzicidrh7vmfyrsgft53qvm-prod
 
 # read
 aws dynamodb scan \
@@ -16,7 +47,7 @@ aws dynamodb scan \
  > "$TABLE_TO-payload.json"
 
 # write
-aws dynamodb batch-write-item --request-items file://"$TABLE_TO-payload.json"
+# aws dynamodb batch-write-item --request-items file://"$TABLE_TO-payload.json"
 
 # clean up
-rm "$TABLE_TO-payload.json"
+# rm "$TABLE_TO-payload.json"
--- 0010_MIGRATION/README.md
+++ 0010_MIGRATION/README.md
@@ -2,6 +2,14 @@
 https://stackoverflow.com/questions/12241161/copying-one-table-to-another-in-dynamodb
 
 
+# PartiQL editor
+```sql
+SELECT *
+FROM "Document-d6sfnut2zrdljfaz3ysbjfhvke-dev"
+WHERE (productFeatureID != '6c558549_c6a0_42dd_a393_60b389b8f2cf' 
+AND productFeatureID != 'a93839e5_5f6f_409e_b04a_5f103fd16417'
+AND productFeatureID != '59022eed_0ed0_4f32_9956_170daed3c2d2')
+```
 # commands
 
 ```sh
