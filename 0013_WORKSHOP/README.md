ab0e5c2fee5a5fc82e67113fe855ded70f45a167
Robin Ochoa <robin8a@gmail.com>
2024-05-03 09:40:09 -0500

    update

---
+++ 0013_WORKSHOP/README.md
@@ -0,0 +1,36 @@
+# DataLake
+- https://docs.aws.amazon.com/whitepapers/latest/building-data-lakes/monitoring-optimizing-data-lake-environment.html
+- https://us-east-1.console.aws.amazon.com/glue/home?region=us-east-1#/v2/data-catalog/databases
+- https://docs.aws.amazon.com/athena/latest/ug/select.html
+
+
+## 1. Documentacion y entregrables (OK)
+- Documento 1.4.x
+- Lista de entregables @sergio
+
+## Datalake Introduction
+- Udemy pdf
+
+## Data Catalog and Querying (last session, continue... today)
+Ohio
+
+Permissions =>  S3 Bucket => Upload Data => Configure Crawler => Add a datastore => Security settings => Specify Output (Database) => Run Crawler => Tables
+
+31. 
+
+
+##  Glue ETL with Apache Sparks (today)
+36.
+1. Create Job (s3 example job)
+- Visual => Change properties
+- Job Details => Role
+
+
+## Next session: S3
+
+## Next2 session: Schema evolution
+
+
+## Oracle
+
+SELECT DAT
---
+++ 0013_WORKSHOP/queries.sql
@@ -0,0 +1,50 @@
+-- Preview Customer Reviews
+SELECT * 
+FROM "amazon_reviews_parquet"
+limit 10;
+
+-- Show distinct sentiments
+SELECT distinct sentiment
+FROM "amazon_reviews_parquet"
+limit 10;
+
+-- Star Rating and Sentiment
+SELECT star_rating, sentiment, count(*) as total_reviews
+FROM "demo_db"."amazon_reviews_parquet"
+group by star_rating, sentiment
+order by star_rating, sentiment;
+
+-- List top rated appliances
+SELECT 
+    product_title, star_rating,
+    sentiment, review_headline, review_body
+FROM "amazon_reviews_parquet"
+order by star_rating desc
+limit 10;
+
+-- Good star rating but sentiment is not positive
+SELECT
+    product_title, star_rating,
+    sentiment, review_headline, review_body
+FROM "amazon_reviews_parquet"
+where star_rating > 3
+and sentiment != 'POSITIVE'
+limit 10;
+
+-- List top rated 
+SELECT
+    product_title, star_rating,
+    sentiment, review_headline, review_body
+FROM "amazon_reviews_parquet"
+where product_category='Major Appliances'
+and star_rating > 3
+and product_title like '%Dryer%'
+limit 10;
+
+
+SELECT TRUNC(DATE_XXX, 'DD')
+FROM my_table
+
+30/04/2024 10:48:19
+
+30/04/2024
